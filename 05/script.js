de3817ee941b1ee6dc334171e74bea051aef4f5d
Diogo Jesus <diogo.joao@tutanota.com>
2022-05-26 20:36:48 +0100

    02-Caixa com butão

--- 05/script.js
+++ 05/script.js
@@ -2,10 +2,20 @@ const caixa = document.getElementById("caixa");
 const botao = document.getElementById("btn");
 let clicks = 0;
 
-botao.addEventListener("click", function() {
-caixa.style.backgroundColor = "yellow" ;
-botao.innerText = "Desligado" ;
-clicks++;
-console.log("Click Botão");
+botao.addEventListener("click", function () {
+
+    clicks++;
+    if (clicks % 2) {
+        caixa.style.backgroundColor = "yellow";
+        botao.innerText = "Desligado";
+    } else {
+        caixa.style.backgroundColor = "blue";
+        botao.innerText = "ligado";
+    }
 
 });
+
+
+
+
+console.log("Click Botão");
